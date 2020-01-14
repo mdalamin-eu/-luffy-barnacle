0abd640451a100d7755d1fc50297b4af3a3f9305
ahmed.iqu <jniqbal1@gmail.com>
2020-01-14 22:32:03 +0600

    Install Server

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const  app = express();
+app.get('/', (req,res) => res.send('Server running'));
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
