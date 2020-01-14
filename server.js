12461563121a312a4df71cefcfc8da45fc79a886
ahmed.iqu <jniqbal1@gmail.com>
2020-01-14 23:24:54 +0600

    Connect Database using Mongo Atlas

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const  app = express();
+// connection database
+connectDB();
+
 app.get('/', (req,res) => res.send('Server running'));
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
