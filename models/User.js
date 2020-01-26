8ede3dc8a5976809d9451c9b6210bf6abd36ccb5
ahmed.iqu <jniqbal1@gmail.com>
2020-01-26 21:42:18 +0600

    Check Validation using Express validator

--- models/User.js
+++ models/User.js
@@ -1,22 +1,22 @@
 const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
-    name:{
+    name: {
         type: String,
         required: true
     },
-    email:{
+    email: {
         type: String,
         required: true,
         unique:true
     },
-    password:{
+    password: {
         type: String,
         required: true
     },
-    avatar:{
+    avatar: {
         type: String
     },
-    date:{
+    date: {
         type: Date,
         default: Date.now
     }
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const router = express.Router();
-
+const { check,validationResult } = require('express-validator/check');
 // @route  Get api/users
 // @desc    Reguster User
 // @access   public 
-router.post('/',(req,res) => {
+router.post(
+    '/',
+[
+    check('name','Name is requried')
+    .not()
+    .isEmpty(),
+    check('email','Email is required').isEmail(),
+    check('password','Please enter a password with 6 or more charracters').isLength({ min : 6})
+],
+(req,res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array() });
+    }
     console.log(req.body);
     res.send('User Route');
 });
