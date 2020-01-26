1890f9d38d59adff65c1b0defabe8e616e7b8563
ahmed.iqu <jniqbal1@gmail.com>
2020-01-27 00:23:42 +0600

    Add jwt

--- config/db.js
+++ config/db.js
@@ -3,7 +3,11 @@ const config = require('config');
 const db = config.get('mongoURI');
 const connectDB = async () => {
     try{
-        await mongoose.connect(db);
+        await mongoose.connect(db, {
+        useNewUrlParser: true,
+        useCreateIndex: true
+        });
+        
         console.log('database connection successfully');
     }catch(err){
         console.error(err.message);
